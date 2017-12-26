4de580a677f8571818045fa36ea7e07ae46011ca
tanjera <ibi.keller@gmail.com>
2017-12-25 21:24:57 -0800

    Rewrite 0.02 - Initial project for OSX added to solution.

---
+++ II_OSX/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace II_OSX
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
