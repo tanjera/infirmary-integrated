655390b53ff4caeb3696312593640e5034285425
Ibi K <ibi.keller@gmail.com>
2019-04-15 23:18:08 -0400

    - II Tools: DictionaryBuilder compiles strings from Excel .xlsx

--- Tools/DictionaryBuilder/DictionaryBuilder.xaml.cs
+++ Tools/DictionaryBuilder/DictionaryBuilder.xaml.cs
@@ -1,18 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
+using Excel = Microsoft.Office.Interop.Excel;
 
 
 namespace DictionaryBuilder {
@@ -22,29 +13,64 @@ namespace DictionaryBuilder {
     public partial class DictionaryBuild : Window {
         public DictionaryBuild () {
             InitializeComponent ();
+
+            // Hard-coded... too lazy to make a file picker
+            txtFilepath.Text = @"C:\Users\Ibi\Documents\Infirmary Integrated\Tools\DictionaryBuilder\bin\Debug\Localization Strings.xlsx";
         }
 
-        private void OnClick_ProcessText (object sender, RoutedEventArgs e) {
-            StringReader srI1 = new StringReader (txtInput1.Text);
-            StringReader srI2 = new StringReader (txtInput2.Text);
-            StringBuilder sbOut = new StringBuilder ();
-            txtOutput.Clear ();
+        private void OnClick_ProcessSpreadsheet(object sender, RoutedEventArgs e) {
+            List<string> Languages = new List<string>();
+            List<Dictionary<string, string>> Dictionaries = new List<Dictionary<string, string>>();
+            StringBuilder sbOut = new StringBuilder();
 
-            string eachI1, eachI2;
-            while ((eachI1 = srI1.ReadLine ()) != null && (eachI2 = srI2.ReadLine ()) != null) {
-                if (eachI1 == "" && eachI2 == "") {
-                    //sbOut.AppendLine ("");
-                    continue;
-                } else if (eachI1.ToUpper () == "KEY") {
-                    sbOut.AppendLine (String.Format ("\t\tstatic Dictionary<string, string> {0} = new Dictionary<string, string> () {{",
-                        eachI2.ToUpper ().Substring (0, 3)));
-                } else
-                    sbOut.AppendLine (String.Format ("\t\t\t{{{0,-60} {1}}},",
-                        String.Format("\"{0}\",", eachI1),
-                        String.Format ("\"{0}\"", eachI2)));
+            txtOutput.Clear();
+
+            Excel.Application xApp = new Excel.Application();
+            Excel.Workbook xWorkbook = xApp.Workbooks.Open(txtFilepath.Text, 0, true, 5, "", "", true, Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+            Excel.Worksheet xWorksheet = (Excel.Worksheet)xWorkbook.Sheets[1];
+
+            Excel.Range xRange = xWorksheet.UsedRange;
+            int rowCount = xRange.Rows.Count;
+            int colCount = xRange.Columns.Count;
+
+            string key;
+            Excel.Range range;
+
+
+            // $B1 -> $...1: language codes
+            for (int j = 2; j <= colCount; j++) {
+                Languages.Add((xWorksheet.Cells[1, j] as Excel.Range).Value.ToString().ToUpper().Substring(0, 3));
+                Dictionaries.Add(new Dictionary<string, string>());
             }
 
-            sbOut.AppendLine ("\t\t};\n");
+            // Iterate all rows, populate respective dictionaries
+            for (int i = 2; i <= rowCount; i++) {
+                range = xWorksheet.Cells[i, 1] as Excel.Range;
+                if (range == null || range.Value == null)
+                    continue;
+
+                key = range.Value.ToString();
+
+                for (int j = 2; j <= colCount; j++) {
+                    range = xWorksheet.Cells[i, j] as Excel.Range;
+                    Dictionaries[j - 2].Add(key, range.Value == null ? "" : range.Value.ToString());
+                }
+            }
+
+            xWorkbook.Close();
+            xApp.Quit();
+
+            // Compile dictionaries into C# localization code
+            for (int i = 0; i < Languages.Count; i++) {
+                sbOut.AppendLine(String.Format("\t\tstatic Dictionary<string, string> {0} = new Dictionary<string, string> () {{", Languages[i]));
+
+                foreach (KeyValuePair<string, string> pair in Dictionaries[i])
+                    sbOut.AppendLine(String.Format("\t\t\t{{{0,-60} {1}}},",
+                        String.Format("\"{0}\",", pair.Key),
+                        String.Format("\"{0}\"", pair.Value)));
+
+                sbOut.AppendLine("\t\t};\n\n");
+            }
 
             txtOutput.Text = sbOut.ToString ();
             Clipboard.SetText (txtOutput.Text);
