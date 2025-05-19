81d6ee0b76166b1c82aca18b22b198273f967279
Ibi Keller <ibi.keller@gmail.com>
2025-05-19 16:27:32 -0400

    IIDT 1.2.5: Publish Self-contained Packages - publish-init.sh: '--sc' flag added to dotnet publish   - Forcing self-contained package compilations   - Previously was automatically self-contained... stopped working sometime     - Needs to be explicitly flagged now?

--- II Development Tools/publish-init.sh
+++ II Development Tools/publish-init.sh
@@ -142,18 +142,18 @@ for arch in ${ARCHITECTURES[*]}; do
     echo -e "${OUT_PREFIX}Building II Simulator for ${arch}\n"
     cd "$DIR_SIMULATOR"
     if [[ $arch == osx* ]]; then
-        dotnet publish -c Release -r $arch -p:UseAppHost=true
+        dotnet publish -c Release --sc -r $arch -p:UseAppHost=true
     else
-        dotnet publish -c Release -r $arch
+        dotnet publish -c Release --sc -r $arch
     fi
 
     # Build II Scenario Editor
     echo -e "${OUT_PREFIX}Building II Scenario Editor for ${arch}\n"
     cd "$DIR_SCENED"
     if [[ $arch == osx* ]]; then
-        dotnet publish -c Release -r $arch -p:UseAppHost=true
+        dotnet publish -c Release --sc -r $arch -p:UseAppHost=true
     else
-        dotnet publish -c Release -r $arch
+        dotnet publish -c Release --sc -r $arch
     fi
 
     # Setup working (/tmp) directory and subdirectories
